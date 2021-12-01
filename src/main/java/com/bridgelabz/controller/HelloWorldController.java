1414d889719ab700395b9009cdb846b273696f65
ashwith31 <ashwitash@gmail.com>
2021-12-01 11:37:06 +0530

    Added: Hello message using MVC controller

--- src/main/java/com/bridgelabz/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,11 @@ public class HelloWorldController {
     @GetMapping("/web")
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model){
+        model.addAttribute("message", "This is a custom message. Hello From bridgelabz");
+        return "message";
     }
 }
