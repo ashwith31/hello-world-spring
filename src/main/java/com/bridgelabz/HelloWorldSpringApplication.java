9f53359d616442510756c06b14f3b618c6aa2340
ashwith31 <ashwitash@gmail.com>
2021-12-01 21:00:13 +0530

    Added: Logger to log the messages using slf4j package

--- src/main/java/com/bridgelabz/HelloWorldSpringApplication.java
+++ src/main/java/com/bridgelabz/HelloWorldSpringApplication.java
@@ -1,13 +1,19 @@
 package com.bridgelabz;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class HelloWorldSpringApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(HelloWorldSpringApplication.class);
+
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		logger.debug("Hello World!!");
 		SpringApplication.run(HelloWorldSpringApplication.class, args);
 	}
 }
